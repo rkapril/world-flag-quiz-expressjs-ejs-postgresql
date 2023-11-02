8a36446a71782d8c6a7ef83a9192dd911ec53de5
Ray Goh <61505106+rkapril@users.noreply.github.com>
2023-11-03 01:14:06 +0800

    Add files via upload

---
+++ Create Table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS captials (
+	id SERIAL PRIMARY KEY,
+	country VARCHAR(45),
+	captial VARCHAR(45)
+);
+
+CREATE TABLE IF NOT EXISTS flags (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(45),
+	flag TEXT
+);
